06fa658435552687f5edf54fc14443623256727d
fatmayaakoubi <93053821+fatmayaakoubi@users.noreply.github.com>
2022-02-12 14:39:39 +0100

    updated

---
+++ script.js
@@ -0,0 +1,66 @@
+
+  
+  // the reset button
+  function resetAnswer() {
+    var answerbox = document.getElementById('answer');
+    answerbox.innerHTML = "Your result will show up here!";
+  }
+
+
+
+
+
+
+  if(document.getElementById("first-a").cheked===true){
+    alert("You are  in great shape congrates.");
+  }else if(document.getElementById("seconed-a").cheked===true){
+    alert("You are  in great shape congrates.")
+  }
+  if(document.getElementById("third-a").cheked===true){
+    alert("You are  in great shape congrates.");
+  } if(document.getElementById("fourth-a").cheked===true){
+    alert("You are  in great shape congrates.")
+  }else if(document.getElementById("fith-a").cheked===true){
+    alert("You are  in great shape congrates.")
+  }
+
+  if(document.getElementById("first-c").cheked===true){
+    alert("you need to take care of your health more check my articals for some ideas");
+  }else if(document.getElementById("seconed-c").cheked===true){
+    alert("you need to take care of your health more check my articals for some ideas")
+  }
+  if(document.getElementById("third-c").cheked===true){
+    alert("you need to take care of your health more check my articals for some ideas");
+  } if(document.getElementById("fourth-c").cheked===true){
+    alert("you need to take care of your health more check my articals for some ideas")
+  }else if(document.getElementById("fith-c").cheked===true){
+    alert("you need to take care of your health more check my articals for some ideas")
+  }
+
+  if(document.getElementById("first-d").cheked===true){
+    alert("You are not healthy you need to stop the bad habits");
+  }else if(document.getElementById("seconed-d").cheked===true){
+    alert("You are not healthy you need to stop the bad habits")
+  }
+  if(document.getElementById("third-d").cheked===true){
+    alert("You are not healthy you need to stop the bad habits");
+  } if(document.getElementById("fourth-d").cheked===true){
+    alert("You are not healthy you need to stop the bad habits")
+  }else if(document.getElementById("fith-d").cheked===true){
+    alert("You are not healthy you need to stop the bad habits")
+  }
+
+
+
+  if(document.getElementById("first-b").cheked===true){
+    alert("You are not healthy Enough");
+  }else if(document.getElementById("seconed-b").cheked===true){
+    alert("You are not healthy Enough")
+  }
+  if(document.getElementById("third-b").cheked===true){
+    alert("You are not healthy Enough");
+  } if(document.getElementById("fourth-b").cheked===true){
+    alert("You are not healthy Enough")
+  }else if(document.getElementById("fith-b").cheked===true){
+    alert("You are not healthy Enough")
+  }
